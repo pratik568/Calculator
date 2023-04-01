6a42bc2f59713d8c0444965104dab9d6106f585d
pratik568 <94685060+pratik568@users.noreply.github.com>
2023-04-01 19:30:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+(function(){
+    let screen=document.querySelector('.screen');
+    let buttons=document.querySelectorAll('.btn');
+    let clear=document.querySelector('.btn-clear');
+    let equal=document.querySelector('.btn-equal');
+    let cut=document.querySelector('.btn-cut');
+    
+
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            let value=e.target.dataset.num;
+            screen.value +=value; 
+        })
+    });
+    equal.addEventListener('click', function(e){
+        if(screen.value === ""){
+            screen.value="";
+        }else{
+            let answer=eval(screen.value);
+            screen.value=answer;
+        }
+    });
+    clear.addEventListener('click',function(e){
+        screen.value="";
+    })
+    cut.addEventListener('click',function(e){
+        let  val = screen.value;
+        screen.value= val.slice(0,-1);
+    })
+})();
